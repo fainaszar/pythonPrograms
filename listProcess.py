46abc1f99ddb88ae774207771f8a7bb890856ae1
Faizan <fainaszar@gmail.com>
2017-10-27 12:39:07 +0530

    Modified listProcess file

--- listProcess.py
+++ listProcess.py
@@ -14,20 +14,35 @@ def processList(lst):
 
 				if lst.count(item) > 0:
 					lst.remove(item)
-
-					
+		else:
+			if lst.count(name) > 1:
+				x = lst.index(name)
+				for i in lst[x+1:]:
+					if i == name:
+						lst.remove(i)
 	return lst
 
 
 
-inpList = ["Bayer Aspirin","aspirin","xelzange","lyrica","Xelzange Maven" ,"Xelzange","aspirin", "Pfizer Aspirin" , "Aspirin"]
+
+
+
+List = ["Bayer Aspirin","aspirin","xelzange ","lyrica","Xelzange Maven" ,"Xelzange","aspirin","Pfizer Aspirin" , "Aspirin", "s","s","s","s" ,"t"]
 #N = int(raw_input("Enter no of list items"))
 
 #for i in range(N):
 #	inpList.append(raw_input())
+inpList=[]
+#print "Input List:\n",List
+#print "\n"
 
-print "Input List:\n",inpList
-print "\n"
+for item in List:
+	inpList.append(item.strip())
+
+print "Input List: \n" ,inpList
+
+
 optList = processList(inpList)
 
 print "Output List:\n",optList
+
